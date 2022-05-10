4950d2583abce367dbf8a0015d289243a1c0282f
dmoran3 <49569041+dmoran3@users.noreply.github.com>
2022-05-10 18:28:57 -0400

    Update cv_test.py

--- FinalProject/cv_test.py
+++ FinalProject/cv_test.py
@@ -66,7 +66,31 @@ def getTrajectory(obj1, obj2):
     traj_y = obj1[1] - obj2[1]
     traj = [traj_x, traj_y]
     return traj
-  
+
+
+'''
+https://pyautogui.readthedocs.io/en/latest/screenshot.html
+
+
+>>> import pyautogui
+>>> button7location = pyautogui.locateOnScreen('calc7key.png')
+>>> button7location
+Box(left=1416, top=562, width=50, height=41)
+>>> button7location[0]
+1416
+>>> button7location.left
+1416
+>>> button7point = pyautogui.center(button7location)
+>>> button7point
+Point(x=1441, y=582)
+>>> button7point[0]
+1441
+>>> button7point.x
+1441
+>>> button7x, button7y = button7point
+>>> pyautogui.click(button7x, button7y)  # clicks the center of where the 7 button was found
+>>> pyautogui.click('calc7key.png') # a shortcut version to click on the center of where the 7 button was found
+'''
 cv2.imshow('Screenshot', scshot)
 
 cv2.waitKey(0)
